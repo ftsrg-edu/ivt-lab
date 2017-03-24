2b9bbe83f71611dfca5b43164b56710d6f28305e
Laszlo Borbely <laci.dj@gmail.com>
2017-03-24 01:05:47 -0700

    implementing launch

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,8 +78,9 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedos
-        //TODO implement feature
-
+        if(!secondaryTorpedoStore.isEmpty() && !primaryTorpedoStore.isEmpty()){
+        	firingSuccess = (secondaryTorpedoStore.fire(1) && primaryTorpedoStore.fire(1));
+        }
         break;
     }
 
