57074d66acdab3d4439c0eb88a7fd25e8d0fc313
meres <okros.daniel.raw@gmail.com>
2017-04-07 00:01:27 -0700

    ALL FIRED DONE

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,8 +77,37 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedos
-        //TODO implement feature
+
+          if (! secondaryTorpedoStore.isEmpty()) {
+            firingSuccess = secondaryTorpedoStore.fire(1);
+            wasPrimaryFiredLast = false;
+          }
+          else {
+            // although primary was fired last time, but the secondary is empty
+            // thus try to fire primary again
+            if (! primaryTorpedoStore.isEmpty()) {
+              firingSuccess = primaryTorpedoStore.fire(1);
+              wasPrimaryFiredLast = true;
+            }
+
+            // if both of the stores are empty, nothing can be done, return failure
+          }
+
+          // try to fire the primary first
+          if (! primaryTorpedoStore.isEmpty()) {
+            firingSuccess = primaryTorpedoStore.fire(1);
+            wasPrimaryFiredLast = true;
+          }
+          else {
+            // although secondary was fired last time, but primary is empty
+            // thus try to fire secondary again
+            if (! secondaryTorpedoStore.isEmpty()) {
+              firingSuccess = secondaryTorpedoStore.fire(1);
+              wasPrimaryFiredLast = false;
+            }
+
+            // if both of the stores are empty, nothing can be done, return failure
+          }
 
         break;
       default:
