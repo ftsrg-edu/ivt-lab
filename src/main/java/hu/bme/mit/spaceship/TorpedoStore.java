8cd7fa057cded83836be1ef06854e0aa41ce458b
Laszlo Borbely <laci.dj@gmail.com>
2017-04-07 00:26:03 -0700

    remove unused variable

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -19,7 +19,7 @@ public class TorpedoStore {
       throw new IllegalArgumentException("numberOfTorpedos");
     }
 
-    boolean success = false;
+    boolean success;
 
     //simulate random overheating of the launcher bay which prevents firing
     double r = generator.nextDouble();
