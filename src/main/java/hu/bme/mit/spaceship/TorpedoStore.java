eacc265f8e3de8a51dba942a9355a27937bebcb5
Zoltan Micskei <micskeiz@mit.bme.hu>
2019-04-02 23:33:52 +0200

    Add clever refactorings

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -4,13 +4,15 @@ import java.util.Random;
 
 /**
 * Class storing and managing the torpedoes of a ship
+*
+* (Deliberately contains bugs.)
 */
 public class TorpedoStore {
 
-  private double FAILURE_RATE = 0.0;
+  // rate of failing to fire torpedos [0.0, 1.0]
+  private double FAILURE_RATE = 0.0; //NOSONAR
 
   private int torpedoCount = 0;
-  private Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
@@ -28,17 +30,18 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("numberOfTorpedos");
+      new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
+    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount -= numberOfTorpedos;
+      this.torpedoCount =- numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
