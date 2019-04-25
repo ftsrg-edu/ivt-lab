271336b1d4b525e4977a6e9c1f7bf70021dbdd27
unknown <tht.krisztian@gmail.com>
2019-04-25 21:13:46 +0200

    Random reused

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,8 +14,11 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
+  Random generator = null;
+
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
+    this.generator = new Random();
 
     // update failure rate if it was specified in an environment variable
     String failureEnv = System.getenv("IVT_RATE");
@@ -36,7 +39,6 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
